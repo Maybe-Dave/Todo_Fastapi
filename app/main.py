7b45dd8ad90dbe744a1dc78bec4c80af5c68ff9d
David Dickson <54110577+Maybe-Dave@users.noreply.github.com>
2024-10-24 17:59:14 +0000

    Moved some files around and made some adjustments to the main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api.routers.todo import todo_router
 from tortoise.contrib.fastapi import register_tortoise
+import uvicorn
 
 app = FastAPI()
 
@@ -17,3 +18,6 @@ register_tortoise(
 @app.get("/")
 def index():
     return{"status": "Todo App is Running"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
